b9a19939aa6f3648d531fc146d147d5c457bf09d
Nikita Averyanov <rac1onal@yandex.ru>
2024-05-07 01:28:26 +0300

    feat: добавить тени основным картинкам

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,18 +8,11 @@ import './Header.css';
 function Header() {
 	return (
 		<div className="header">
-			{/* <div className="header__logo-wrapper">
-			</div> */}
 			<Logo />
+
 			<div className="header__link">
 				<OurCoffeeLink />
 			</div>
-			{/* <Link
-				className="header__link link"
-				to="/our-coffee">
-				Our Coffee
-			</Link> */}
-
 		</div>
 	);
 };
